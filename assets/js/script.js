6ae4f6ad7a0e05cfaac7fe067cc57ecbeedbc243
gtoffa <guille.toffa@gmail.com>
2023-02-27 21:28:53 -0300

    Correciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 const urlApi = 'https://randomuser.me/api/?nat=es';
 let currentSuggest;
 var currenPage = 0;
+let currenCv;
+let listFF = [];
+
+
+
 //al cargar la pagina traemos un usuario random y 3 user
 loadPage();
 
@@ -10,46 +15,64 @@ function loadPage() {
 
 
     //Cagar Cv principal
+
     document.getElementById("loading").style.visibility = "inline";
-    fetch(urlApi)
-        .then(response => response.json())
-        .then(data => setDataToCv(data.results[0]));
+
+    currenCv = JSON.parse(localStorage.getItem('currenCv')) ? JSON.parse(localStorage.getItem('currenCv')) : null;
+
+    if (currenCv == null) {
+        fetch(urlApi)
+            .then(response => response.json())
+            .then(data => setDataToCv(data.results[0]));
 
 
-    //Carga de Cv sugeridos
-    loadItemSugest(1);
-
+        //Carga de Cv sugeridos
+    } else {
+        setDataToCv(currenCv);
+    }
+    loadItemSugest(localStorage.getItem('page') ? localStorage.getItem('page') : 1);
 
 
 
 }
+
 
 
 
 
 function setDataToCv(resulData) {
+    currenCv = resulData;
 
-    const user = resulData;
 
     //seteo de valores a las elementos html
-    document.getElementById('name').innerHTML = user.name.first;
-    document.getElementById('lastname').innerHTML = user.name.last;
-    document.getElementById('dob').innerHTML = 'Edad ' + user.dob.age;
+    document.getElementById('name').innerHTML = currenCv.name.first;
+    document.getElementById('lastname').innerHTML = currenCv.name.last;
+    document.getElementById('dob').innerHTML = 'Edad ' + currenCv.dob.age;
 
-    document.getElementById('cell').innerHTML = user.cell;
-    document.getElementById('email').innerHTML = user.email;
-    document.getElementById('street').innerHTML = user.location.street.name + " " + user.location.street.number + " " + user.location.city;
 
-    document.getElementById('image').src = user.picture.large;
+
+    document.getElementById('cell').innerHTML = currenCv.cell;
+    document.getElementById('cell').setAttribute("href", "tel:" + currenCv.cell);
+    document.getElementById('email').innerHTML = currenCv.email;
+    document.getElementById('email').setAttribute("href", "mailto:" + currenCv.email);
+
+    document.getElementById('street').innerHTML = currenCv.location.street.name + " " + currenCv.location.street.number + " " + currenCv.location.city;
+    document.getElementById('street').setAttribute("href", "http://maps.google.com/?q=" + currenCv.location.street.name + " " + currenCv.location.street.number + " " + currenCv.location.city);
+
+    document.getElementById('image').src = currenCv.picture.large;
 
 
     document.getElementById("loading").style.display = "none";
+
+    readFF();
+    localStorage.setItem('currenCv', JSON.stringify(currenCv));
+
 
 }
 
 //Carga la pagina
 function setPage(el) {
-    
+
     const parentNode = el.parentNode;
 
     let btsPagination = document.getElementsByClassName("page-item");
@@ -67,7 +90,9 @@ function setPage(el) {
                     parentNode.setAttribute('class', 'page-item disabled');
                 }
 
-                loadItemSugest(pag);
+                if (pag >= 1) {
+                    loadItemSugest(pag);
+                }
                 break;
             }
         case 'Siguiente':
@@ -76,8 +101,10 @@ function setPage(el) {
                 if (pag == 3) {
                     parentNode.setAttribute('class', 'page-item disabled');
                 }
+                if (pag <= 3) {
 
-                loadItemSugest(pag);
+                    loadItemSugest(pag);
+                }
                 break;
             }
         default:
@@ -92,6 +119,8 @@ function setPage(el) {
 
 
 function loadItemSugest(page) {
+    localStorage.setItem('page', page);
+
 
     //se muestra el div de carga
     document.getElementById("loadingSuggest").style.visibility = "visible";
@@ -141,8 +170,8 @@ function createItemSugest(data) {
     });
     document.getElementById("loadingSuggest").style.visibility = "hidden";
 
- 
-    
+
+
 }
 
 function createDivSuggest(element) {
@@ -155,3 +184,72 @@ function createDivSuggest(element) {
 
 }
 
+
+function addFF() {
+    listFF.push(currenCv);
+    localStorage.setItem('favorites', JSON.stringify(listFF));
+    loadFFMenu();
+
+}
+
+function removeFF() {
+
+    listFF = listFF.filter(item => item.id.value !== currenCv.id.value);
+    localStorage.setItem('favorites', JSON.stringify(listFF));
+    loadFFMenu();
+
+
+}
+
+function readFF() {
+    listFF = JSON.parse(localStorage.getItem('favorites')) ? JSON.parse(localStorage.getItem('favorites')) : [];
+    loadFFMenu();
+
+}
+
+
+
+function loadFFMenu() {
+
+
+    document.getElementById('menuff').innerHTML = '';
+
+
+
+    listFF.forEach(element => {
+        const itemSuggest = document.createElement("li");
+
+        itemSuggest.innerHTML = createLinkFF(element);
+        itemSuggest.addEventListener("click", function () { window.scrollTo({ top: 0, behavior: 'smooth' }); setDataToCv(element); });
+        document.getElementById('menuff').appendChild(itemSuggest);
+
+    });
+
+    vertificarButtonFF();
+}
+
+function createLinkFF(element) {
+
+    return `<a class="dropdown-item" style="cursor: grab;" >
+    <img src="${element.picture.large}"
+    class="bd-placeholder-img rounded-circle" width="30" height="30"  
+    alt="..."/>&nbsp
+    ${element.name.last}  ${element.name.first}
+    
+    </a>`;
+
+}
+
+function vertificarButtonFF() {
+    btnFF = document.getElementById("btnFF");
+
+    if (listFF.filter(item => item.id.value == currenCv.id.value).length > 0) {
+        btnFF.innerHTML = `<span> Favoritos <i class="material-icons">favorite</i> </span>`;
+        btnFF.setAttribute('class', 'btn-flotante-ff');
+        btnFF.onclick = function () { removeFF() }
+    } else {
+        btnFF.innerHTML = `Favoritos <I class="material-icons">favorite_border</I>`;
+        btnFF.setAttribute('class', 'btn-flotante');
+        btnFF.onclick = function () { addFF() }
+    }
+}
